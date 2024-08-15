c1f38bb73bf5cf127cd75041e1542385fa2738e1
nycpetite24 <nycpetite2@washedlih.com>
2024-08-14 22:46:32 -0400

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        goldText: '#A17C5E',
+      },
+      fontFamily: {
+        crimson: ['Crimson Text', 'serif'],
+      },
+      content: {
+        'star': 'url("/assets/redstar.svg")',
+      },
+    },
+  },
+};
